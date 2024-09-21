a4011d45a395f1bf7d9888a7f09065e847b8624f
amusedrook <12457881+amusedrook@users.noreply.github.com>
2024-09-21 09:26:36 +0100

    Autocommit from 2024-09-21 09:26:36

---
+++ autocommit.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Based on:
+# https://github.com/EricZimmerman/VoronTools/blob/main/autocommit.sh
+# Trimmed to bar-bones
+
+# Path to config folder
+config_folder=~/printer_data/config
+
+# Repo branch (default: main)
+branch=main
+
+push_backup(){
+  cd $config_folder
+  git pull origin $branch --no-rebase
+  git add .
+  current_date=$(date +"%Y-%m-%d %T")
+  git commit -m "Autocommit from $current_date"
+  git push origin $branch
+}
+
+push_backup
